2f85851e60dc0de710053d6661bff9e5b22ab217
Jackline Muraya <jacklinewanja.jw@gmail.com>
2023-01-04 12:56:40 +0300

    Low memory cost
    Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+LockedClass class
+"""
+
+
+class LockedClass:
+    """ setattr """
+    __slots__ = ["first_name"]
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
